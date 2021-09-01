b7bf77bb7f4e553dba336d2caaf1fa7f8408b0d8
Dark_Shadow04 <32217440+DarkShadow04@users.noreply.github.com>
2021-09-01 20:35:59 +0530

    Add files via upload

--- youtube_video_download.py
+++ youtube_video_download.py
@@ -1,5 +1,18 @@
-from pytube import Youtube
-link = input("Enter URL")
-video = Youtube(link)
-stream = video.streams.get_highest_resolution()
-stream.download()
+from pytube import YouTube
+
+#ask for the link from user
+link = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+#Getting the highest resolution possible
+ys = yt.streams.get_highest_resolution()
+
+#Starting download
+print("Downloading...")
+ys.download()
+print("Download completed!!")
