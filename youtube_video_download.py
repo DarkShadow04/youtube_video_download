8ca5318d145d9000e5d8758c99ae44488696f033
Dark_Shadow04 <32217440+DarkShadow04@users.noreply.github.com>
2021-09-01 20:36:35 +0530

    Update youtube_video_download.py

--- youtube_video_download.py
+++ youtube_video_download.py
@@ -9,6 +9,7 @@ print("Title: ",yt.title)
 print("Number of views: ",yt.views)
 print("Length of video: ",yt.length)
 print("Rating of video: ",yt.rating)
+
 #Getting the highest resolution possible
 ys = yt.streams.get_highest_resolution()
 
